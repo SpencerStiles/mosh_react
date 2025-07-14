a5adc6419e0965948339852f3de83bce27720431
Spencer Stiles <spencer.stiles12@gmail.com>
2025-07-13 22:24:15 -0600

    setup commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div className="alert alert-primary">
+      <Alert>
+        Hello <span> test </span> World!
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
