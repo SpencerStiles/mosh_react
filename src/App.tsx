9c4c6806d225ead29cf7d5d74df8e9ec9bea8045
Spencer Stiles <spencer.stiles12@gmail.com>
2025-07-14 12:53:40 -0400

    finished creating components exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
-    <div className="alert alert-primary">
-      <Alert>
-        Hello <span> test </span> World!
-      </Alert>
+    <div>
+      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
